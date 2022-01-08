669d05c1faf7150b6a3339568bd846a5d79843ad
onuragtas <agtasonur@gmail.com>
2022-01-08 11:49:03 +0300

    run on go func

--- cron.go
+++ cron.go
@@ -11,23 +11,24 @@ type Cron struct {
 }
 
 type cron struct {
-	Cron ICron
-	Schedule time.Duration
+	Cron      ICron
+	Schedule  time.Duration
 	LastStart time.Time
 }
 
 func (c *Cron) Run() {
-	for {
-		for key, item := range c.cronList {
-			if time.Now().Sub(item.LastStart) > item.Schedule {
-				go item.Cron.Start()
-				c.cronList[key].LastStart = time.Now()
+	go func() {
+		for {
+			for key, item := range c.cronList {
+				if time.Now().Sub(item.LastStart) > item.Schedule {
+					go item.Cron.Start()
+					c.cronList[key].LastStart = time.Now()
+				}
 			}
 		}
-	}
+	}()
 }
 
 func (c *Cron) Add(iCron ICron, duration time.Duration) {
 	c.cronList = append(c.cronList, cron{Cron: iCron, Schedule: duration})
 }
-
